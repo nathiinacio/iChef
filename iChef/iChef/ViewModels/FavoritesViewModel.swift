4007f8babe74d12d36efd14e334c51dbae518047
Nathalia Inacio <nathi_1997@hotmail.com>
2024-01-17 11:52:56 -0300

    :sparkles: Favorite screen done

---
+++ iChef/iChef/ViewModels/FavoritesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesViewModel.swift
+//  iChef
+//
+//  Created by Nathalia Inacio on 17/01/24.
+//
+
+import Foundation
---
+++ iChef/iChef/Views/FavoritesView.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritesVIew.swift
+//  iChef
+//
+//  Created by Nathalia Inacio on 16/01/24.
+//
+
+import Foundation
